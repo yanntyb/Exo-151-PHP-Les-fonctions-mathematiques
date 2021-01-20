8ade027e9c394e6f41c5c71f4dce6674d0dfcce3
Nux007 <007.nux@gmail.com>
2021-01-20 08:22:06 +0100

    Exo 151.

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * 1. Affichez dans un <p> la valeur absolue de -8
+ */
+$value = -8;
+// TODO Votre code ici.
+
+
+/**
+ * 2. Affichez dans un <p> la valeur de 5.64 arrondit au nombre supérieur.
+ */
+$value = 5.64;
+// TODO Votre code ici.
+
+
+/**
+ * 3. Affichez dans un <p> la valeur de 5.34 arrondit au nombre inférieur.
+ */
+$value = 5.34;
+// TODO Votre code ici.
+
+
+/**
+ * 4. Affichez dans un <p> la valeur maximum du tableau $max, et la valeur minimum du tableau $min.
+ */
+$max = [5, 10, 12, 8, 7, 6, 54, 39, 76, 63, 84, 2, 21];
+$min = [5, 41, 65, 4, 3, 65, 2, 65, 74, 51, 23, 39, 42];
+// TODO Votre code ici.
+
+
+/**
+ * 5. Affichez dans un <p> la valeur de PI * 2 ( avec une fonction mathématique pour PI ).
+ */
+
+// TODO Votre code ici.
+
+
+/**
+ * 6. Affichez dans un <p> la valeur arrondie de 5.42 avec la fonction round().
+ */
+$value = 5.42;
+// TODO Votre code ici.
+
+
+
+
+
